39f389cdee5d051ff84338b089380001d15880d9
Adriano Rosa <info@adrianorosa.com>
2021-05-08 11:38:53 -0300

    setup heroku

--- server/server.js
+++ server/server.js
@@ -11,11 +11,9 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  dataSources: () => {
-    return {
-      usersAPI: new UserAPI(),
-    };
-  },
+  dataSources: () => ({
+    usersAPI: new UserAPI(),
+  })
 });
 
 server.applyMiddleware({ app });
@@ -25,22 +23,24 @@ app.use(
   morgan('dev')
 );
 
-// this route mimic a REST API that we can use as DataSourceREST for GraphQL
+// This route mimic a REST API that we can use as DataSourceREST for GraphQL
 app.get('/api/users', async (req, res) => {
   // await new Promise(r => setTimeout(r, 3000));
   res.json(require('./db/users.json'));
 });
 
 // This route will serve the production version of React App within the client/build directory
-app.use('/', express.static(path.join(__dirname, '../client/build')));
+// it was just defined to make it ease to deploy this stack at heroku
+app.use('/', express.static(path.join(__dirname, 'build')));
 
-// Fallback route
+// Fallback route that's display 404
 app.use((req, res) => {
   res.status(404);
   res.send('Not Found');
   res.end();
 });
 
-app.listen({ port: 4000 }, () => {
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is ready at http://localhost:${port}`);
 });
